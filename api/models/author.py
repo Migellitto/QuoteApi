5f7d8db25df2d94c8ae2692dce8516010ffbb32b
Mikhail <mz-mail@mail.ru>
2023-01-25 14:01:27 +0300

    recreate DB + surname and rating

--- api/models/author.py
+++ api/models/author.py
@@ -4,11 +4,13 @@ from api import db
 class AuthorModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(32))
+    surname = db.Column(db.String(32), server_default="Testovidze")
     quotes = db.relationship(
         'QuoteModel', backref='author', lazy='dynamic', cascade='all, delete')
 
-    def __init__(self, name):
+    def __init__(self, name, surname):
         self.name = name
+        self.surname = surname
 
     # def to_dict(self):
     #     return {
--- migrations/versions/849332c2ee4b_new_db.py
+++ migrations/versions/849332c2ee4b_new_db.py
@@ -1,8 +1,8 @@
-"""empty message
+"""new db
 
-Revision ID: f6eda7049057
+Revision ID: 849332c2ee4b
 Revises: 
-Create Date: 2021-12-13 07:10:31.616002
+Create Date: 2023-01-25 13:55:55.181682
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'f6eda7049057'
+revision = '849332c2ee4b'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -21,12 +21,14 @@ def upgrade():
     op.create_table('author_model',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('name', sa.String(length=32), nullable=True),
+    sa.Column('surname', sa.String(length=32), server_default='Testovidze', nullable=True),
     sa.PrimaryKeyConstraint('id')
     )
     op.create_table('quote_model',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('author_id', sa.Integer(), nullable=True),
     sa.Column('text', sa.String(length=255), nullable=True),
+    sa.Column('rating', sa.Integer(), server_default='1', nullable=True),
     sa.ForeignKeyConstraint(['author_id'], ['author_model.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
